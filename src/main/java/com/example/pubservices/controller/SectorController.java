ce4192a6fd1a357d46560ccbb03af35db5aafea6
infopluscabrel <jtano@olydis.com>
2022-06-15 15:26:45 +0200

    fin crud back-end

---
+++ src/main/java/com/example/pubservices/controller/SectorController.java
@@ -0,0 +1,47 @@
+package com.example.pubservices.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.pubservices.model.Sector;
+import com.example.pubservices.service.SectorService;
+
+@RestController
+@RequestMapping("sector")
+public class SectorController {
+    
+    @Autowired
+    private SectorService sectorService;
+
+    @PostMapping(path = "/add")
+    public ResponseEntity<Sector> saveSector(@RequestBody Sector sector){
+        return new ResponseEntity<>(this.sectorService.saveSector(sector), HttpStatus.CREATED);
+    }
+
+    @GetMapping(path = "/all")
+    public ResponseEntity<List<Sector>> getAllSectors(){
+        return new ResponseEntity<>(this.sectorService.getAllSector(), HttpStatus.OK);
+    }
+
+    @PutMapping(path="/update")
+    public ResponseEntity<Sector> updateUser(@RequestParam Long id, @RequestBody Sector sector) {
+        sector.setIdSector(id);
+        return new ResponseEntity<>(this.sectorService.updateSector(sector), HttpStatus.OK);
+    }
+
+    @DeleteMapping("/delete")
+    public void delete(@RequestParam Long idSector){
+        this.sectorService.deleteSector(idSector);
+    }
+}
--- src/main/java/com/example/pubservices/service/SectorService.java
+++ src/main/java/com/example/pubservices/service/SectorService.java
@@ -1,8 +1,11 @@
 package com.example.pubservices.service;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.example.pubservices.model.Sector;
 import com.example.pubservices.repository.SectorRepository;
 
 @Service
@@ -10,4 +13,20 @@ public class SectorService {
     
     @Autowired
     private SectorRepository sectorRepository;
+
+    public Sector saveSector(Sector sector){
+        return this.sectorRepository.save(sector);
+    }
+
+    public List<Sector> getAllSector(){
+        return this.sectorRepository.findAll();
+    }
+
+    public Sector updateSector(Sector sector){
+        return this.sectorRepository.save(sector);
+    }
+
+    public void deleteSector(Long idSector){
+        this.sectorRepository.deleteById(idSector);
+    }
 }
