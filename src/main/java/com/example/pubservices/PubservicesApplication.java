cd001592ad0440dcdf61b5d72172a4dd94317155
infopluscabrel <jtano@olydis.com>
2022-06-15 13:23:14 +0200

    fin implementation des modeles

---
+++ src/main/java/com/example/pubservices/PubservicesApplication.java
@@ -0,0 +1,13 @@
+package com.example.pubservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PubservicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PubservicesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/pubservices/PubservicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.pubservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PubservicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
